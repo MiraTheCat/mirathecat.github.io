a5339f65c936df947f94c84a479d47f325cef0e9
MiraTheCat <83456953+MiraTheCat@users.noreply.github.com>
2021-05-31 21:12:13 +0200

    discord embed update

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,22 @@
-console.log("Hello, World!");
-var test = "test";
-console.log(test);
-test = "test2";
-console.log(test);
+var peanut = 234;
+var money = 0;
+console.log("You have " + peanut + " peanut(s) and " + money + " coins!");
+
+function farm(amount) {
+	peanut += amount
+  console.log("You just farmed " + amount + " peanut(s) and now have " + peanut + " peanut(s) in total!");
+}
+
+function sell(amount) {
+	if (peanut >= amount) {
+		peanut -= amount
+ 		money += amount * 0.01
+ 		console.log("You just sold " + amount + " peanut(s)!");
+ 		console.log("You now have " + peanut + " peanut(s) and " + money + " coins!");
+	} else {
+  	console.log("You don't have " + amount + " peanut(s)!");
+  }
+}
+
+farm(22);
+sell(123);
