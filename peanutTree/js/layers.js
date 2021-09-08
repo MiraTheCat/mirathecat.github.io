7728df88f9f538af0c6d49acea0a1b1f6e5d4691
MiraTheCat <83456953+MiraTheCat@users.noreply.github.com>
2021-09-08 20:04:24 +0200

    e

--- peanutTree/js/layers.js
+++ peanutTree/js/layers.js
@@ -7,6 +7,7 @@ addLayer("c", {
 		points: new Decimal(1),
         total: new Decimal(0),
         best: new Decimal(0),
+        auto: false,
     }},
     color: "#d5d900",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
@@ -25,6 +26,9 @@ addLayer("c", {
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
+    },
+    passiveGeneration() {
+        return (hasMilestone("sg", 2)) ? 1 : 0
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
@@ -129,24 +133,23 @@ addLayer("c", {
 addLayer("f", {
     name: "Farms", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "F", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: false,
 		points: new Decimal(0),
         total: new Decimal(0),
         best: new Decimal(0),
+        auto: false,
     }},
     color: "#0a9100",
     requires() {
-        return new Decimal(2500)
+        return new Decimal(2500).times((!player.f.unlocked && player.sg.unlocked) ? 1000 : 1)
     }, // Can be a function that takes requirement increases into account
     resource: "farms", // Name of prestige currency
     baseResource: "peanuts", // Name of resource prestige is based on
     branches: ["c"],
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type() {
-        if (hasMilestone("f", 1))
-            return "normal"
         return "static"
     },
     exponent: 1.5, // Prestige currency exponent
@@ -155,7 +158,7 @@ addLayer("f", {
         return mult
     },
     canBuyMax() {
-        return false
+        return hasMilestone("f", 1)
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
@@ -165,7 +168,7 @@ addLayer("f", {
         {key: "f", description: "F: Perform a farm reset", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown() {
-        return hasUpgrade('c', 23) || player[this.layer].total
+        return player.c.unlocked
     },
     addToBase() {
         let base = new Decimal(0);
@@ -206,6 +209,13 @@ addLayer("f", {
             },
             effectDescription: "You can buy max Farms",
         },
+        2: {
+            requirementDescription: "10 Farms",
+            done() {
+                return player.f.best.gte(10)
+            },
+            effectDescription: "Unlock more Coin upgrades",
+        },
     },
 
     upgrades: {
@@ -215,7 +225,7 @@ addLayer("f", {
             cost: new Decimal(2),
 
             unlocked() {
-                return player.f.unlocked
+                return player.c.unlocked
             },
 
             effect() {
@@ -226,17 +236,16 @@ addLayer("f", {
         },
 
         12: {
-            title: "Farm Workers",
-            description: "Workers add to the Farm effect base",
+            title: "Farm Generators",
+            description: "Sapling Generators add to the Farm effect base",
             cost: new Decimal(4),
 
             unlocked() {
-                return player.f.unlocked && false // player.sg.unlocked
+                return player.f.unlocked && player.sg.unlocked
             },
 
             effect() {
-                let ret = 1;
-                // let ret = player.sg.points.add(1).log10().sqrt().div(3);
+                let ret = player.sg.points.add(1).log10().sqrt().div(3);
                 return ret
             },
             effectDisplay() { return "+" + format(upgradeEffect(this.layer, this.id)) }, // Add formatting to the effect
@@ -248,7 +257,7 @@ addLayer("f", {
             cost: new Decimal(7),
 
             unlocked() {
-                return player.f.unlocked
+                return player.c.unlocked
             },
 
             effect() {
@@ -257,5 +266,146 @@ addLayer("f", {
             },
             effectDisplay() { return "+" + format(upgradeEffect(this.layer, this.id)) }, // Add formatting to the effect
         },
+
+        21: {
+            title: "Faster-Growing Saplings",
+            description: "Square the Sapling effect",
+            cost: new Decimal(9),
+
+            unlocked() {
+                return hasUpgrade(this.layer, 12) && player.sg.unlocked
+            },
+
+            effect() {
+                let ret = player.sg.points.add(1).log10().sqrt().div(3);
+                return ret
+            },
+        },
+
+        22: {
+            title: "Farm Discount",
+            description: "Farms are cheaper based on your peanuts",
+            cost: new Decimal(12),
+
+            unlocked() {
+                return hasUpgrade(this.layer, 13)
+            },
+
+            effect() {
+                let ret = player.points.add(1).log10().add(1).pow(3.2);
+                return ret;
+            },
+            effectDisplay() {return "/" + format(tmp.f.upgrades[22].effect)}, // Add formatting to the effect
+        },
+    },
+})
+
+addLayer("sg", {
+    name: "Sapling Generators", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "SG", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+        total: new Decimal(0),
+        best: new Decimal(0),
+        saplings: new Decimal(0),
+        auto: false,
+    }},
+    color: "#696969",
+    requires() {
+        return new Decimal(2500).times((!player.sg.unlocked && player.f.unlocked) ? 1000 : 1)
+    },
+    resource: "sapling generators", // Name of prestige currency
+    baseResource: "peanuts", // Name of resource prestige is based on
+    branches: ["c"],
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type() {
+        return "static"
+    },
+    exponent: 1.5, // Prestige currency exponent
+    gainMult() {
+        let mult = new Decimal(1)
+        return mult
+    },
+    canBuyMax() {
+        return hasMilestone("sg", 1)
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "s", description: "S: Perform a Sapling Generator reset", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown() {
+        return player.c.unlocked
+    },
+
+    addToBase() {
+        let base = new Decimal(0);
+        return base;
+    },
+    effBase() {
+        let base = new Decimal(2);
+        return base;
+    },
+    effect() {
+        if ((!player.sg.unlocked))
+            return new Decimal(0);
+        let eff = Decimal.pow(this.effBase(), player.g.points.plus(tmp.sg.spectralTotal)).sub(1).max(0);
+        return eff;
+    },
+    effectDescription() {
+        return "which are generating " + format(tmp.sg.effect) + " Saplings/sec"
+    },
+    update(diff) {
+        if (player.sg.unlocked)
+            player.sg.saplings = player.sg.saplings.plus(tmp.sg.effect.times(diff));
+    },
+    powerExp() {
+        let exp = new Decimal(1 / 3);
+        if (hasUpgrade("f", 21))
+            exp = exp.times(2);
+        return exp;
+    },
+    powerEff() {
+        if (!player.sg.unlocked)
+            return new Decimal(1);
+        return player.sg.power.plus(1).pow(this.powerExp());
+    },
+    tabFormat: ["main-display", "prestige-button", "blank", ["display-text", function() {
+        return 'You have ' + format(player.sg.saplings) + ' Saplings, which boosts Peanut production by ' + format(tmp.sg.powerEff) + 'x'
+    }
+    , {}], "blank", ["display-text", function() {
+        return 'Your best Sapling Generators is ' + formatWhole(player.sg.best) + '<br>You have made a total of ' + formatWhole(player.sg.total) + " Sapling Generators."
+    }
+    , {}], "blank", "milestones", "blank", "blank", "upgrades"],
+
+    milestones: {
+        0: {
+            requirementDescription: "5 Sapling Generators",
+            done() {
+                return player.sg.best.gte(5)
+            },
+            effectDescription: "Keep Coin upgrades on reset",
+        },
+        1: {
+            requirementDescription: "8 Sapling Generators",
+            done() {
+                return player.sg.best.gte(8)
+            },
+            effectDescription: "You can buy max Sapling Generators",
+        },
+        2: {
+            requirementDescription: "10 Sapling Generators",
+            done() {
+                return player.sg.best.gte(10)
+            },
+            effectDescription: "You gain 100% of Coin gain every second",
+        },
+    },
+
+    upgrades: {
     },
 })
