66d6880580192a6f09c0f12c78357cb6047c02b1
MiraTheCat <83456953+MiraTheCat@users.noreply.github.com>
2021-09-11 15:53:16 +0200

    Update layers.js

--- peanutTree/js/layers.js
+++ peanutTree/js/layers.js
@@ -75,7 +75,7 @@ addLayer("c", {
             },
 
             effect() {
-                return player[this.layer].points.add(1).pow(0.3)
+                return player.c.points.plus(2).pow(0.2);
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
         },
