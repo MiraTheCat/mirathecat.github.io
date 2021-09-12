53ff8a34f3c66aa44914b0bda1fd793ddae19aac
MiraTheCat <83456953+MiraTheCat@users.noreply.github.com>
2021-09-12 14:36:19 +0200

    Update mod.js

--- peanutTree/js/mod.js
+++ peanutTree/js/mod.js
@@ -19,7 +19,7 @@ let VERSION = {
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.1</h3><br>
-		- Added Boosters & Generators<br>`
+		- Added Farms & Sapling Generators<br>`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
