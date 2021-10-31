c9682d639db6c2236508786574353cc1561f7205
MiraTheCat <83456953+MiraTheCat@users.noreply.github.com>
2021-10-31 14:28:49 +0100

    Update peanutgame.js

--- javascript/peanutgame.js
+++ javascript/peanutgame.js
@@ -1424,7 +1424,7 @@ function updateInventory(peanuts1, money1, peanutsPerClick1, peanutsPerSecond1) 
 		prestigeButton.innerHTML = "Prestige";
 		prestigeButton.style.background = "linear-gradient(180deg, rgb(86, 0, 107), rgb(19, 0, 24))";
 	} else {
-		prestigeButton.innerHTML = "To Prestige: $" + prestigeRequirement;
+		prestigeButton.innerHTML = "To Prestige: $" + roundNumber(prestigeRequirement);
 		prestigeButton.style.background = "linear-gradient(180deg, rgb(109, 109, 109), rgb(19, 0, 24))";
 	}
 }
