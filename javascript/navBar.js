5724b116b94d52328c834e73ff82c90e4d0e50eb
MiraTheCat <83456953+MiraTheCat@users.noreply.github.com>
2021-11-07 14:46:00 +0100

    Update navBar.js

--- javascript/navBar.js
+++ javascript/navBar.js
@@ -5,21 +5,25 @@ function addNavBar() {
     navBarEl.appendChild(homeEl);
     homeEl.href = "/index.html";
     homeEl.classList.add("top", "bordered");
+    homeEl.innerHTML = "Home";
 
     let aboutEl = document.createElement("a");
     navBarEl.appendChild(aboutEl);
     aboutEl.href = "/about.html";
     aboutEl.classList.add("top", "bordered");
+    aboutEl.innerHTML = "About";
 
     let projectsEl = document.createElement("a");
     navBarEl.appendChild(projectsEl);
     projectsEl.href = "/projects.html";
     projectsEl.classList.add("top", "bordered");
+    projectsEl.innerHTML = "Projects";
 
     let gamesEl = document.createElement("a");
     navBarEl.appendChild(gamesEl);
     gamesEl.href = "/games.html";
     gamesEl.classList.add("top", "bordered");
+    gamesEl.innerHTML = "Games";
 }
 
 addNavBar();
